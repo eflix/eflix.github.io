5ac6f3174a47b1458bf0c45709e28367dec03adb
eflix <lekdarmawan99@gmail.com>
2022-05-22 00:00:51 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,61 @@
+// event link di klik
+$('.page-scroll').on('click', function(e){
+	
+	// ambil isi href
+	var tujuan = $(this).attr('href');
+
+	// tangkap elemen yg bersangkutan
+	var elementTujuan = $(tujuan);
+
+	// pindahkan scroll
+	$('html, body').animate({
+		scrollTop:elementTujuan.offset().top - 50
+	}, 1250, 'swing');
+
+	e.preventDefault();
+});
+
+
+// menyimpan paralax
+
+$(window).on('load', function(){
+	$('.pKiri').addClass('pMuncul');
+	$('.pKanan').addClass('pMuncul');
+});
+
+$(window).scroll(function(){
+	var wScroll = $(this).scrollTop();
+
+	// jumbotron
+	$('.jumbotron img').css({
+		'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+	});
+
+	$('.jumbotron h1').css({
+		'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+	});
+
+	$('.jumbotron p').css({
+		'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
+	});
+
+	// portfolio
+	if (wScroll > $('.portfolio').offset().top - 250 ) {
+		$('.portfolio .thumbnail').each(function(i){
+			setTimeout(function(){
+				$('.portfolio .thumbnail').eq(i).addClass('muncul');	
+			}, 300 *(i+1));
+		});
+
+		
+	}
+
+	if (wScroll > $('.skills').offset().top - 250) {
+		$('.sbContent').addClass('animasiIn');
+		console.log('muncul');
+	} else {
+		$('.sbContent').remove('animasiIn');
+	}
+
+	//console.log(wScroll);
+});
